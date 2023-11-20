5661800905fb7967b0af286e5835556b5e544b67
Khalid Kaiser <romman.kaiser@gmail.com>
2023-11-20 23:16:12 +0600

    client-5

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,68 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useAuthContext } from "../customHooks/useAuthContext";
+import { useLogout } from "../customHooks/useLogout";
 
 function Navbar() {
-    const location = useLocation();
+  const location = useLocation();
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
+  const [dropDownOpen, setDropDownOpen] = React.useState(false);
 
+  const toggleDropdown = () => {
+    setDropDownOpen(!dropDownOpen);
+  };
+  const handleClick = () => {
+    logout();
+  };
 
   return (
-    <div className='header'>
+    <div className="header">
       <div className="container">
         <div className="topbar">
-            <Link to='/'><img src="" alt="Logo" /></Link>
-            {location.pathname!=='/login' && <Link className='btn' to="/login">Login/Register</Link>}
+          <Link to="/">
+            <img src="" alt="Logo" />
+          </Link>
+          {(location.pathname !== "/login") && (
+            <Link className="btn" to="/login">
+              Login/Register
+            </Link>
+          )} 
+          {user &&
+        <div className="dropdown">
+          <div className="d-flex align-items-center drop" onClick={toggleDropdown}>
+            <span>User Name</span>
+            <span className="material-symbols-rounded">
+              arrow_drop_down
+            </span>
+          </div>
+          <div className={`dropdown-content ${dropDownOpen ? "show" : ""}`}>
+            <Link className="nav-link" to="/profile">
+              <span className="material-symbols-outlined mr-2">  
+                account_circle
+              </span>
+              My Profile
+            </Link>
+            <Link className="nav-link" to="/profile">
+              <span className="material-symbols-outlined mr-2">  
+                account_circle
+              </span>
+              My Profile
+            </Link>
+            <button
+              className="dropbtn d-flex align-items-center"
+              onClick={handleClick}
+            >
+              <span className="material-symbols-outlined mr-2">logout</span>
+              Logout
+            </button>
+          </div>
+        </div>
+        }
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 function Home() {
+  const [paste, setPaste] = useState('')
+  const [title, setTitle] = useState('')
+  const [category, setCategory] = useState('')
+  const [expiration, setExpiration] = useState('')
+  const [privacy, setPrivacy] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleSubmit = () => {
     alert('You clicked the button!')
@@ -10,28 +16,28 @@ function Home() {
     <div className='home'>
       <div className="left">
         <h3 className='mb-3'>New Paste</h3>
-        <textarea name="paste" id="paste" cols="30" rows="10"></textarea>
+        <textarea name="paste" id="paste" cols="30" rows="10" onChange={(e) => setPaste(e.target.value)}></textarea>
         <div className="optional mt-3">
           <h3>Optional Settings</h3>
           <div className="input-group">
-            <label htmlFor="catagory">Title:</label>
-            <input type="text" name='title'/>
+            <label htmlFor="title">Title:</label>
+            <input type="text" name='title' onChange={(e) => setTitle(e.target.value)}/>
           </div>
           <div className="input-group">
-            <label htmlFor="catagory">Catagory:</label>
-            <input type="text" name='catagory'/>
+            <label htmlFor="category">Category:</label>
+            <input type="text" name='category' onChange={(e) => setCategory(e.target.value)}/>
           </div>
           <div className="input-group">
             <label htmlFor="expiration">Expiration:</label>
-            <input type="text" name='expiration'/>
+            <input type="text" name='expiration' onChange={(e) => setExpiration(e.target.value)}/>
           </div>
           <div className="input-group">
             <label htmlFor="privacy">Privacy:</label>
-            <input type="text" name='privacy'/>
+            <input type="text" name='privacy' onChange={(e) => setPrivacy(e.target.value)}/>
           </div>
           <div className="input-group">
             <label htmlFor="catagory">Password:</label>
-            <input type="password" name='password'/>
+            <input type="password" name='password' onChange={(e) => setPassword(e.target.value)}/>
           </div>
           <button className='btn' onClick={handleSubmit}>Create New Paste</button>
         </div>
