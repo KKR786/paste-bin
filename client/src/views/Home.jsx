b0480f99049a44ca07ba61895820f12112ba5f1d
Khalid Kaiser <romman.kaiser@gmail.com>
2023-12-05 23:02:10 +0600

    update-13

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import { useAuthContext } from "../customHooks/useAuthContext"
 
 function Home() {
   const [paste, setPaste] = useState('')
@@ -8,39 +9,87 @@ function Home() {
   const [expiration, setExpiration] = useState('')
   const [privacy, setPrivacy] = useState('')
   const [password, setPassword] = useState('')
+  const [success, setSuccess] = useState('')
 
-  const handleSubmit = () => {
-    alert('You clicked the button!')
+  const { user } = useAuthContext();
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    if (!user) {
+      localStorage.setItem('Paste', JSON.stringify({
+        paste,
+        title,
+        category,
+        expiration,
+        privacy,
+        password,
+      }));
+      return window.location.replace("/login")
+    } else {
+      try {
+        const response = await fetch("/api/paste/new", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user.token}`,
+          },
+          body: JSON.stringify({
+            paste,
+            title,
+            category,
+            expiration,
+            privacy,
+            password,
+          }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+          }
+        if (response.ok) {
+          const json = await response.json();
+          setSuccess('New Paste Created')
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
   return (
     <div className='home'>
       <div className="left">
         <h3 className='mb-3'>New Paste</h3>
-        <textarea name="paste" id="paste" cols="30" rows="10" onChange={(e) => setPaste(e.target.value)}></textarea>
+        <form onSubmit={handleSubmit}>
+        <textarea name="paste" id="paste" cols="30" rows="10" onChange={(e) => setPaste(e.target.value)} required></textarea>
         <div className="optional mt-3">
           <h3>Optional Settings</h3>
           <div className="input-group">
             <label htmlFor="title">Title:</label>
-            <input type="text" name='title' onChange={(e) => setTitle(e.target.value)}/>
+            <input type="text" name='title' onChange={(e) => setTitle(e.target.value)} required/>
           </div>
           <div className="input-group">
             <label htmlFor="category">Category:</label>
-            <input type="text" name='category' onChange={(e) => setCategory(e.target.value)}/>
+            <input type="text" name='category' onChange={(e) => setCategory(e.target.value)} required/>
           </div>
           <div className="input-group">
             <label htmlFor="expiration">Expiration:</label>
-            <input type="text" name='expiration' onChange={(e) => setExpiration(e.target.value)}/>
+            <input type="text" name='expiration' onChange={(e) => setExpiration(e.target.value)} required/>
           </div>
           <div className="input-group">
             <label htmlFor="privacy">Privacy:</label>
-            <input type="text" name='privacy' onChange={(e) => setPrivacy(e.target.value)}/>
+            <input type="text" name='privacy' onChange={(e) => setPrivacy(e.target.value)} required/>
           </div>
           <div className="input-group">
             <label htmlFor="catagory">Password:</label>
-            <input type="password" name='password' onChange={(e) => setPassword(e.target.value)}/>
+            <input type="password" name='password' onChange={(e) => setPassword(e.target.value)} required/>
           </div>
-          <button className='btn' onClick={handleSubmit}>Create New Paste</button>
+          <button className='btn'>Create New Paste</button>
         </div>
+        </form>
+        {success &&
+          <p>{success}</p>
+        }
       </div>
       <div className="right">
         <h3 className='mb-3'>Public Pastes</h3>
